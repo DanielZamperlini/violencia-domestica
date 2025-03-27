5b7ae5dfed76249fea7f1637b585c8c8afe18f16
Daniel Zamperlini <danielzamperlini.developer@gmail.com>
2025-03-26 22:23:49 -0300

    click dropdown-menu

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ botao.addEventListener('click', function () {
   const menu = document.querySelector('.menu');
   menu.classList.toggle('open');
 });
+
+const dropdownOpen = document.querySelectorAll('.dropdown');
+
+dropdownOpen.forEach(function (item) {
+  item.addEventListener('click', function () {
+    this.querySelector('.dropdown-menu').classList.toggle('dropdown-menu-open');
+  });
+});
+
+document.addEventListener('click', function (event) {
+  // Fecha o dropdown se o clique for fora do dropdown
+  const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+  dropdownMenus.forEach(function (dropdownMenu) {
+    if (!dropdownMenu.closest('.dropdown').contains(event.target)) {
+      dropdownMenu.classList.remove('dropdown-menu-open');
+    }
+  });
+});
