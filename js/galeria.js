3dae200e1dc30e1de68facdc244dc44864a10a34
Daniel Zamperlini <danielzamperlini.developer@gmail.com>
2025-04-30 22:48:57 -0300

    botao de prev e next na galeria

--- js/galeria.js
+++ js/galeria.js
@@ -1,15 +1,24 @@
-// galeria
 document.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('imageModal');
   const modalImg = document.getElementById('modalImage');
   const closeButton = document.querySelector('.close-button');
-  const galleryItems = document.querySelectorAll('.gallery-item img');
+  const nextButton = document.querySelector('.next-button');
+  const prevButton = document.querySelector('.prev-button');
+  const galleryItems = Array.from(
+    document.querySelectorAll('.gallery-item img'),
+  );
+  let currentIndex = 0;
 
-  function openModal(imgSrc, imgAlt) {
+  function openModal(imgSrc, imgAlt, index) {
     modal.classList.add('active');
     modalImg.src = imgSrc.replace('w=800', 'w=1200');
     modalImg.alt = imgAlt;
     document.body.style.overflow = 'hidden';
+    currentIndex = index;
+
+    // Mostrar os botões de navegação
+    prevButton.style.display = 'block';
+    nextButton.style.display = 'block';
   }
 
   function closeModal() {
@@ -17,15 +26,34 @@ document.addEventListener('DOMContentLoaded', () => {
     modalImg.src = '';
     modalImg.alt = '';
     document.body.style.overflow = '';
+
+    // Esconder os botões de navegação
+    prevButton.style.display = 'none';
+    nextButton.style.display = 'none';
   }
 
-  galleryItems.forEach((img) => {
+  function showNextImage() {
+    currentIndex = (currentIndex + 1) % galleryItems.length;
+    const nextImage = galleryItems[currentIndex];
+    openModal(nextImage.src, nextImage.alt, currentIndex);
+  }
+
+  function showPrevImage() {
+    currentIndex =
+      (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+    const prevImage = galleryItems[currentIndex];
+    openModal(prevImage.src, prevImage.alt, currentIndex);
+  }
+
+  galleryItems.forEach((img, index) => {
     img.addEventListener('click', () => {
-      openModal(img.src, img.alt);
+      openModal(img.src, img.alt, index);
     });
   });
 
   closeButton.addEventListener('click', closeModal);
+  nextButton.addEventListener('click', showNextImage);
+  prevButton.addEventListener('click', showPrevImage);
 
   modal.addEventListener('click', (e) => {
     if (e.target === modal) {
@@ -36,6 +64,10 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
       closeModal();
+    } else if (e.key === 'ArrowRight') {
+      showNextImage();
+    } else if (e.key === 'ArrowLeft') {
+      showPrevImage();
     }
   });
 });
